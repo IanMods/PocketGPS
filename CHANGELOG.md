ce908d90edb00ccc21704e9caf9cf08b9432ffc3
Ian Anderson <lanAnderson@users.noreply.github.com>
2024-08-16 20:14:43 -0700

    Finished with 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,12 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 
-## [1.0.0] - 2024-08-13
+## [1.0.1] - 2024-08-16
+
+### Added
+-
+
+## [1.0.0] - 2024-08-16
 
 ### Added
 
--- common/src/main/java/club/iananderson/pocketgps/items/GpsItem.java
+++ common/src/main/java/club/iananderson/pocketgps/items/GpsItem.java
@@ -5,7 +5,6 @@ import club.iananderson.pocketgps.minimap.CurrentMinimap.Minimaps;
 import java.util.List;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.item.Item;
@@ -23,20 +22,20 @@ public class GpsItem extends Item {
     List<Minimaps> currentMinimaps = CurrentMinimap.loadedMinimaps();
     MutableComponent loadedMinimap;
 
-    if(currentMinimaps.isEmpty()){
+    if (currentMinimaps.isEmpty()) {
       loadedMinimap = Component.translatable("minimap.pocketgps.none");
+    } else {
+      loadedMinimap = currentMinimaps.get(0).getModName();
     }
-    else loadedMinimap = currentMinimaps.get(0).getModName();
 
-    if (Screen.hasShiftDown()){
+    if (Screen.hasShiftDown()) {
       tooltip.add(Component.translatable("item.pocketgps.gps.tooltip").withStyle(ChatFormatting.GRAY));
       tooltip.add(Component.literal(""));
-      tooltip.add(Component.translatable("item.pocketgps.gps.tooltip.minimap.current")
-                      .withStyle(ChatFormatting.YELLOW)
+      tooltip.add(Component.translatable("item.pocketgps.gps.tooltip.minimap.current").withStyle(ChatFormatting.YELLOW)
                       .append(loadedMinimap.withStyle(ChatFormatting.AQUA)));
+    } else {
+      tooltip.add(Component.translatable("item.pocketgps.gps.tooltip.default").withStyle(ChatFormatting.YELLOW));
     }
-
-    else tooltip.add(Component.translatable("item.pocketgps.gps.tooltip.default").withStyle(ChatFormatting.YELLOW));
 
     super.appendHoverText(stack, level, tooltip, flag);
   }
